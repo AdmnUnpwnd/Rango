182ae032e767f98d14ba246d2f321ae0ffe901b2
AdmnUnpwnd <admnunpwnd@users.noreply.github.com>
2014-12-30 04:26:56 -0600

    Added Rango App Index

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World! ~ Rango <br/> <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("About Rango <br/> <a href='/rango/'>Index</a>")
+
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -5,6 +6,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')),
 )
+
